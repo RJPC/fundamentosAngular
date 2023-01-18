a7b815a82f0ea8b3c1496846c5e4ceaff801f1fb
Rahiber Paradas <rahiber.paradas@gmail.com>
2023-01-18 10:42:13 -0400

    Clase 8 y 9 - Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Rahiber';
-  private age = 32;
+  private age = 5;
   img = "https://source.unsplash.com/random/"
+  progressMax = 100;
+  disabledIncreaseAge = false;
+  disabledDecreaseAge = false;
 
   getAge(){
     return this.age
   }
 
   btnDisabled = true;
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge(){
+    if(this.age < this.progressMax){
+      this.age += 1;
+    }
+
+    if(this.age == 100){
+      this.disabledIncreaseAge = true;
+    }
+
+    if(this.age > 0){
+      this.disabledDecreaseAge = false;
+    }
+  }
+
+  decreaseAge(){
+    if(this.age > 0) {
+      this.age -= 1;
+    }
+
+    if(this.age == 0){
+      this.disabledDecreaseAge = true;
+    }
+
+    if(this.age < 100){
+      this.disabledIncreaseAge = false;
+    }
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changesName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.name = element.value;
+    console.log(element.value);
+  }
 }
