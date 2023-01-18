db93af86027735e3fbcc9dbe059f39a524e1c796
Rahiber Paradas <rahiber.paradas@gmail.com>
2023-01-18 16:24:36 -0400

    Experiment with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,59 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = 'Rahiber';
-  age = 5;
-  img = "https://source.unsplash.com/random/"
-  progressMax = 100;
-  disabledIncreaseAge = false;
-  disabledDecreaseAge = false;
 
-  getAge(){
-    return this.age
+  name: string = '';
+  category: string = '';
+  quantity: number = 0;
+
+  product = {
+    name: '',
+    category: '',
+    quantity: 0
   }
 
-  btnDisabled = true;
+  products = [{name: 'Tomate', category: 'Perecederos', quantity: 10}]
 
-  toggleButton(){
-    this.btnDisabled = !this.btnDisabled;
+  addProduct(){
+    this.products.push(this.product)
   }
 
-  increaseAge(){
-    if(this.age < this.progressMax){
-      this.age += 1;
-    }
-
-    if(this.age == 100){
-      this.disabledIncreaseAge = true;
-    }
-
-    if(this.age > 0){
-      this.disabledDecreaseAge = false;
-    }
-  }
-
-  decreaseAge(){
-    if(this.age > 0) {
-      this.age -= 1;
-    }
-
-    if(this.age == 0){
-      this.disabledDecreaseAge = true;
-    }
-
-    if(this.age < 100){
-      this.disabledIncreaseAge = false;
-    }
-  }
-
-  onScroll(event: Event){
-    const element = event.target as HTMLElement;
-    console.log(element.scrollTop);
-  }
-
-  changesName(event: Event){
-    const element = event.target as HTMLInputElement;
-    this.name = element.value;
-    console.log(element.value);
-  }
 }
